7f6fdd107a9eb9b132d02bc432d61aff96651752
Mubashir-salafi <148326059+Mubashir-salafi@users.noreply.github.com>
2023-12-13 10:42:50 +0100

    Add files via upload

---
+++ nettside.Diagram/index.js
@@ -0,0 +1,7 @@
+// index.js
+
+function validateLogin() {
+  // Regardless of the entered username and password, redirect the user
+  // Replace 'https://your-other-website-url.com' with the actual URL of your other website
+  window.location.href = 'file:///C:/Users/mubas/OneDrive%20-%20Viken%20fylkeskommune/vsc/Ny%20mappe/index.html';
+}
